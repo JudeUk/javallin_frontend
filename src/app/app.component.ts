54e6ece10ecf8a483759c19d8cbcf3eb8603db12
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-03-04 15:43:53 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'javellin-frontend';
+
+  data={
+    courseTitle :'Javellin website'
+  }
 }
