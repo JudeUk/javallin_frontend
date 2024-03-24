2ce6348fd5dcbef3c295bf764804879d83576913
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-03-24 16:04:56 +0000

    route landing page to legal lens application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InsightsComponent } from './insights/insights.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 
 export const routes: Routes = [
-    {path: '', component: DashboardComponent},
+    {path: '', component: LandingpageComponent},
+    {path:'dashboard', component: DashboardComponent},
     { path: 'insights', component: InsightsComponent },
 ];
 
