734afa08414f5eb6f632320018ef0abfa9fdb046
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-03-27 09:52:33 +0000

    backend api call added, file uploaded added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { InsightsComponent } from '../insights/insights.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [InsightsComponent,RouterModule],
+  imports: [InsightsComponent,RouterModule,HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
+
+
 export class DashboardComponent {
+
+  constructor(private http: HttpClient) {}
+  caseFile : any;
+
+getFile(event: any) {
+
+this.caseFile = event.target.files[0];
+console.log('file has been uploaded>>>>>>>',this.caseFile)
+
+}
+
+  
+
+  uploadFile() {
+
+    const formData = new FormData();
+    formData.set('file', this.caseFile);
+    
+    return this.http.post('http://127.0.0.1:8000/upload/', formData).subscribe((response) => {});
+  }
 
 }
