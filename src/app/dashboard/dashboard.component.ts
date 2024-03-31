da4a4c206ad57e41ea5c31e7557bb77c7241e412
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-03-31 20:57:57 +0000

    file upload added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,11 +17,37 @@ export class DashboardComponent {
 
   constructor(private http: HttpClient) {}
   caseFile : any;
-
+  formData = new FormData();
 getFile(event: any) {
 
 this.caseFile = event.target.files[0];
+
 console.log('file has been uploaded>>>>>>>',this.caseFile)
+
+  
+
+  // Extract button label for differentiation
+  // const buttonLabel = event.target.parentElement.textContent.trim();
+  const buttonLabel = event.target.parentElement.querySelector('label').textContent.trim();
+
+  // Append selected file based on button label
+  switch (buttonLabel) {
+
+    case '+ Upload facts of Case':
+      this.formData.append('facts of case', this.caseFile, this.caseFile.name);
+      break;
+    case '+ Upload Case Issues':
+      this.formData.append('issues', this.caseFile, this.caseFile.name);
+      break;
+    case '+ Upload Case Arguments':
+      this.formData.append('arguments', this.caseFile, this.caseFile.name);
+      break;
+    case '+ Upload Reference Material':
+      this.formData.append('reference material', this.caseFile, this.caseFile.name);
+      break;
+    default:
+      console.warn('Unexpected button label:', buttonLabel);
+  }
 
 }
 
@@ -29,10 +55,11 @@ console.log('file has been uploaded>>>>>>>',this.caseFile)
 
   uploadFile() {
 
-    const formData = new FormData();
-    formData.set('file', this.caseFile);
+    // const formData = new FormData();
+    // formData.set('file', this.caseFile);
     
-    return this.http.post('http://127.0.0.1:8000/upload/', formData).subscribe((response) => {});
+    
+    return this.http.post('http://127.0.0.1:8000/upload/', this.formData).subscribe((response) => {});
   }
 
 }
