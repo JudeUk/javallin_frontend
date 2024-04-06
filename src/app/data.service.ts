0b1f2d3068766582bfa9579ff16600399e0d0c7b
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-04-06 19:00:57 +0000

    some fixes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   dataToPass: any;
+
+  private similarFilesSource = new BehaviorSubject<any[]>([]);
+
+  similarFiles = this.similarFilesSource.asObservable();
+
+  setSimilarFiles(files:any){
+    this.similarFilesSource.next(files)
+  }
+
 
   constructor() { }
 }
--- src/app/insights/insights.component.ts
+++ src/app/insights/insights.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { DataService } from '../data.service';
 import { SidenavComponent } from "../sidenav/sidenav.component";
@@ -5,7 +6,7 @@ import { SidenavComponent } from "../sidenav/sidenav.component";
 @Component({
   selector: 'app-insights',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [SidenavComponent,CommonModule],
   templateUrl: './insights.component.html',
   styleUrl: './insights.component.css'
 })
@@ -15,16 +16,22 @@ export class InsightsComponent {
   casesWithSimilarArguments: number = 0;
   casesWhereSucceeded: number = 0;
 
+  similarFiles : any = [];
+
   constructor(private dataService: DataService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     // Retrieve data from the DataService
-    const data = this.dataService.dataToPass;
-    if (data) {
-      this.casesWithSimilarFacts = data.casesWithSimilarFacts;
-      this.casesWithSimilarArguments = data.casesWithSimilarArguments;
-      this.casesWhereSucceeded = data.casesWhereSucceeded;
-    }
+    // const data = this.dataService.dataToPass;
+
+this.dataService.similarFiles.subscribe(files => this.similarFiles = files)
+
+console.log(">>>>>>>>>>>>>>THIS IS THE Similarfile field"+ this.similarFiles)
+    // if (data) {
+    //   this.casesWithSimilarFacts = data.casesWithSimilarFacts;
+    //   this.casesWithSimilarArguments = data.casesWithSimilarArguments;
+    //   this.casesWhereSucceeded = data.casesWhereSucceeded;
+    // }
   }
 
 }
