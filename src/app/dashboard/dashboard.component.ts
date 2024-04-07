4c5f227d98fd104fc247e54dbd2a886f13c78f30
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-04-07 00:29:27 +0000

    fixing up mix up merge landing_page to main

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,6 +24,7 @@ const backend_url_production = 'https://legal-legal-vkjha.ondigitalocean.app/upl
 
 export class DashboardComponent {
 
+
   // casesWithSimilarFacts: number = 105;
   // casesWithSimilarArguments: number = 50;
   // casesWhereSucceeded: number = 10;
@@ -36,14 +37,12 @@ export class DashboardComponent {
 
 this.caseFile = event.target.files[0];
 
+  
 
-  // getting the files
-getFile(event: any) {
- this.caseFile = event.target.files[0];
- console.log('file has been uploaded>>>>>>>',this.caseFile)
   // Extract button label for differentiation
   // const buttonLabel = event.target.parentElement.textContent.trim();
   const buttonLabel = event.target.parentElement.querySelector('label').textContent.trim();
+
   // Append selected file based on button label
   switch (buttonLabel) {
 
@@ -105,5 +104,6 @@ uploadFile() {
     }
   );
 }
+
 
 }
