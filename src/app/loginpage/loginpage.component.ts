61b6ca449baed86bece85f6b5228aa3b80a61a68
sav <savelinnjeri@gmail.com>
2024-04-17 21:46:25 +0000

    set up sign in with google

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -4,6 +4,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
 import {faEnvelopeOpen} from '@fortawesome/free-solid-svg-icons';
 import {faLockOpen} from '@fortawesome/free-solid-svg-icons';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { environment } from '../../../environment/environment';
 
 
 @Component({
@@ -17,5 +20,27 @@ export class LoginpageComponent {
 faUser = faUser
 faEnvelopeOpen = faEnvelopeOpen
 faLockOpen = faLockOpen
+
+private auth: any;
+  router: any;
+
+ngOnInit() {
+  const app = initializeApp(environment.firebaseConfig); // Initialize Firebase app
+  this.auth = getAuth(app); // Get Authentication service
+}
+
+signInWithGoogle() {
+  const googleProvider = new GoogleAuthProvider();
+  signInWithPopup(this.auth, googleProvider)
+    .then((result) => {
+      // User signed in successfully
+      const user = result.user;
+      this.router.navigate(['/uploadfiles']);
+      console.log('Logged in user:', user);
+    })
+    .catch((error) => {
+      console.error('Sign in error:', error);
+    });
+}
 
 }
