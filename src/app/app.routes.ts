6d161621537a3082a144a5fbb2656e1960c46f1f
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-04-18 09:28:40 +0000

    added route for chatgpt Jude

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
 import { UploadpageComponent } from './uploadpage/uploadpage.component';
 import { ChatboxComponent } from './chatbox/chatbox.component';
 import { QuerypageComponent } from './querypage/querypage.component';
+import { ChatGptComponent } from './chat-gpt/chat-gpt.component';
 
 
 export const routes: Routes = [
@@ -13,6 +14,7 @@ export const routes: Routes = [
     {path:'dashboard', component: DashboardComponent},
     { path: 'insights', component: InsightsComponent },
     { path: 'upload', component: UploadpageComponent },
+    { path: 'chatgpt', component: ChatGptComponent },
     { path: 'chatbox', component: ChatboxComponent },
     { path: 'querypage', component: QuerypageComponent},
 ];
