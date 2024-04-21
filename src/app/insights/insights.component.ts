4474854d5c531f4d3cda7485d8e0821fa9592a52
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-04-21 20:12:01 +0000

    insights

--- src/app/insights/insights.component.ts
+++ src/app/insights/insights.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { SidenavComponent } from "../sidenav/sidenav.component";
+// import { Component } from '@angular/core';
+// import { SidenavComponent } from "../sidenav/sidenav.component";
 
-@Component({
-  selector: 'app-insights',
-  standalone: true,
-  imports: [SidenavComponent],
-  templateUrl: './insights.component.html',
-  styleUrl: './insights.component.css'
-})
-export class InsightsComponent {
+// @Component({
+//   selector: 'app-insights',
+//   standalone: true,
+//   imports: [SidenavComponent],
+//   templateUrl: './insights.component.html',
+//   styleUrl: './insights.component.css'
+// })
+// export class InsightsComponent {
 
-}
+// }
