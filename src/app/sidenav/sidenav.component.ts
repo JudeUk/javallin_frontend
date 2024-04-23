5e7eefc79a938ec144ecf458e89a887fb3937533
sav <savelinnjeri@gmail.com>
2024-04-23 12:23:17 +0000

    modified search component

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faCloudArrowUp} from '@fortawesome/free-solid-svg-icons';
 import {faChartLine} from '@fortawesome/free-solid-svg-icons';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons';
 import {faComment} from '@fortawesome/free-solid-svg-icons';
 import { RouterModule } from '@angular/router';
@@ -18,4 +19,5 @@ export class SidenavComponent {
   faChartLine = faChartLine
   faRightFromBracket = faRightFromBracket
   faComment = faComment
+  faMagnifyingGlass = faMagnifyingGlass
 }
