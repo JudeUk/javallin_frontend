9bb9ee5c82c57dde4161c7ee7881bc648f537a1a
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-05-03 10:34:22 +0000

    upload component and html modified

--- src/app/uploadpage/uploadpage.component.ts
+++ src/app/uploadpage/uploadpage.component.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { DataService } from '../data.service';
 
 const backend_url_production = 'https://legal-legal-vkjha.ondigitalocean.app/upload/'
+// const backend_url_local = 'http://127.0.0.1:8000/upload/'
 
 @Component({
     selector: 'app-uploadpage',
@@ -18,48 +19,89 @@ export class UploadpageComponent {
 
     selectedFiles: { [key: string]: FileList | null } = {}; // Dictionary to store selected files
 
+    caseFile: any;
+    formData = new FormData();
+
     constructor(private http: HttpClient,private dataService: DataService) { }
   
-    onFileSelected(event: Event, key: string) {
-      const input = event.target as HTMLInputElement;
-      if (input.files) {
-        this.selectedFiles[key] = input.files;
-      } else {
-        this.selectedFiles[key] = null;  // Set back to null if no files selected
-      }
-    }
+    // onFileSelected(event: Event, key: string) {
+    //   const input = event.target as HTMLInputElement;
+    //   if (input.files) {
+    //     this.selectedFiles[key] = input.files;
+    //   } else {
+    //     this.selectedFiles[key] = null;  // Set back to null if no files selected
+    //   }
+    // }
   
-    uploadFiles() {
-      const formData = new FormData();
+    // uploadFiles() {
+    //   const formData = new FormData();
       
-      // Loop through each key in the selectedFiles dictionary
-      for (const key in this.selectedFiles) {
-        const files = this.selectedFiles[key];
-        if (files) {
-          // Append each file using the original key
-          for (let i = 0; i < files.length; i++) {
-            formData.append(key, files[i], files[i].name);
-            // console.log('heeeyyyyyy')
-          }
-        }
-      }
+    //   // Loop through each key in the selectedFiles dictionary
+    //   for (const key in this.selectedFiles) {
+    //     const files = this.selectedFiles[key];
+    //     if (files) {
+    //       // Append each file using the original key
+    //       for (let i = 0; i < files.length; i++) {
+    //         formData.append(key, files[i], files[i].name);
+    //         // console.log('heeeyyyyyy')
+    //       }
+    //     }
+    //   }
   
-      this.http.post<any>(backend_url_production, formData)
-        .subscribe(response => {
-          console.log('Upload successful!', response);
+    //   this.http.post<any>(backend_url_local, formData)
+    //     .subscribe(response => {
+    //       console.log('Upload successful!', response);
+    //       this.dataService.setUploadResponse(response);
+    //       // Handle successful upload response
+    //     }, error => {
+    //       console.error('Upload failed:', error);
+    //       // Handle upload error
+    //     });
+    // }
+
+
+
+
+    getFile(event: any) {
+      this.caseFile = event.target.files[0];
+      this.formData.append('myfile', this.caseFile, this.caseFile.name);
+   }
+  
+   uploadFiles() {
+    const backend_url_local = 'http://127.0.0.1:8000/upload/'
+      this.http.post<any>(backend_url_local, this.formData).subscribe(
+        (response) => {
+          console.log("Response from backend:", response.message);
           this.dataService.setUploadResponse(response);
-          // Handle successful upload response
-        }, error => {
-          console.error('Upload failed:', error);
-          // Handle upload error
-        });
-    }
+        },
+        (error) => {
+          console.error('Error occurred:', error);
+        }
+      );
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     onSubmit(event: Event) {
         // Prevent default form submission behavior (refresh)
         event.preventDefault();
       
-        // Call your method to handle file upload using FormData (existing logic)
+        
         this.uploadFiles(); // Assuming you have a method named uploadFiles()
       }
 }
