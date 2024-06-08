902ee593b2ec66563d514013596f0e2c6d29faff
sav <savelinnjeri@gmail.com>
2024-06-08 17:32:35 +0000

    started working on the side nav to make it responsive

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -6,15 +6,22 @@ import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons';
 import {faComment} from '@fortawesome/free-solid-svg-icons';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [FontAwesomeModule,RouterModule],
+  imports: [FontAwesomeModule,RouterModule,CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
+  isNavOpen = false;
+
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
+
   faCloudArrowUp = faCloudArrowUp
   faChartLine = faChartLine
   faRightFromBracket = faRightFromBracket
